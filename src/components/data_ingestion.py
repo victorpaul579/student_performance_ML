a7cadb734a4a79ebc4a271e29039623e33445468
victorpaul579 <victorpaul579@gmail.com>
2023-04-27 23:46:15 +0530

    Data Ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -36,6 +36,12 @@ class DataIngestion:
 
             logging.info("Ingestion of the data is completed")
             
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+
         except Exception as e:
             raise CustomException(e,sys)
 
